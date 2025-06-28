39a5efc5ed840e78425007dc365e2443855cb985
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-06-27 22:48:51 -0300

    feat: :sparkles: structure profile screen

--- lib/src/screens/profile/profile_screen.dart
+++ lib/src/screens/profile/profile_screen.dart
@@ -5,6 +5,11 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Perfil"),
+      ),
+      body: const Center(child: Text("Conteúdo"),),
+    );
   }
 }
