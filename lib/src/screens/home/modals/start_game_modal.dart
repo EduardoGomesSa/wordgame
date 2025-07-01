10344b3a23e6e1a7e21bfa434d70f3fc66ea8ffc
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-06-30 23:03:27 -0300

    feat: :sparkles: level  options created

--- lib/src/screens/home/modals/start_game_modal.dart
+++ lib/src/screens/home/modals/start_game_modal.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
-class StartGameModal extends StatelessWidget {
+class StartGameModal extends StatefulWidget {
   const StartGameModal({super.key});
 
   @override
+  State<StartGameModal> createState() => _StartGameModalState();
+}
+
+class _StartGameModalState extends State<StartGameModal> {
+  String? selectedLevel;
+  final List<String> levels = ['Fácil', 'Médio', 'Difícil'];
+
+  @override
   Widget build(BuildContext context) {
-    return const Dialog(
-      child: Text("Escolha o nível"),
+    return Dialog(
+      child: Column(
+        children: [
+          const Text("Escolha o nível"),
+          const SizedBox(height: 8),
+          DropdownButton<String>(
+            isExpanded: true,
+            value: selectedLevel,
+            hint: const Text("Selecione"),
+            items: levels.map((level){
+              return DropdownMenuItem<String>(
+                value: level,
+                child: Text(level),);
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                selectedLevel = value;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
