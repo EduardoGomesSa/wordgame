156a0f88ff6146047e9ea94fac784669e36aaf82
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-04 23:39:31 -0300

    feat: :sparkles: structure modal insert a new word

---
+++ lib/src/screens/home/modals/new_word_modal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewWordModal extends StatelessWidget {
+  const NewWordModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
