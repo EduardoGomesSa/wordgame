03b86fa17aafc5beca1172026d4c38aa4b4ef86a
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-07-22 21:02:02 -0300

    feat: :sparkles: creation method static to connect with sqlite

--- lib/src/repositories/database/db.dart
+++ lib/src/repositories/database/db.dart
@@ -1,3 +1,16 @@
-class Db {
-  
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+abstract class Db {
+    static Future<sql.Database> connection() async {
+        WidgetsFlutterBinding.ensureInitialized();
+
+        final dbPath = await sql.getDatabasesPath();
+
+        return sql.openDatabase(path.join(dbPath, 'word_game'),
+        );
+    }
+    
 }
